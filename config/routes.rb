44b53c1bc9a9903eedc875d53ea3f5b46f0f4f59
Talha Butt <talhabuttt@gmail.com>
2020-10-22 19:58:16 +0500

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "players" => "players#index"
+  # get "players" => "players#index"
   get "players/new" => "players#new"
   post "players" => "players#create"
   get "players/:id" => "players#show"
   get "players/:id/edit" => "players#edit"
   patch "players/:id" => "players#update"
+  root 'players#index'
 end
